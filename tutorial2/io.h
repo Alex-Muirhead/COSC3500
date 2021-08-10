e9522c22e12480f031d559dbfd029ebf6337e52d
Ian McCulloch <ianmcc@physics.uq.edu.au>
2021-08-10 17:31:35 +1000

    Cleanup header file and remove spurious dependencies from Makefile

--- tutorial2/io.h
+++ tutorial2/io.h
@@ -1,5 +1,15 @@
+/* COSC3500
+   Example header file io.h to enable separate complation of the I/O functions
+   Written by a previous tutor
+   This comment added Ian McCulloch Semester 2, 2021
+*/
+
+#ifndef COSC3500_IO_H
+
 #define N               100
 #define T               100
 
-
 int mat2hdf5 (double wdata[N][T], char filename[], char dataset[]);
+
+#endif
+
