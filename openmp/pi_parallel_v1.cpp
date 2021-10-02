9ac4ad610fc986a4a65a65459c36e6bf21682d99
Alexander Muirhead <s4435062@fac-login-1.local>
2021-10-02 11:18:11 +1000

    Tweaking openmp tutorial

--- openmp/pi_parallel_v1.cpp
+++ openmp/pi_parallel_v1.cpp
@@ -6,15 +6,12 @@ int main()
    int const num_steps = 100000;
    double const step = 1.0 / num_steps;
    double sum = 0;
-   #pragma omp parallel
+   #pragma omp parallel for reduction (+:sum)
+   for (int i = 0; i < num_steps; ++i)
    {
-      #pragma omp for
-      for (int i = 0; i < num_steps; ++i)
-      {
-         double xi = (i+0.5)*step;
-         double f = 1.0 / (xi*xi+1.0);
-         sum += f;
-      }
+      double xi = (i+0.5)*step;
+      double f = 1.0 / (xi*xi+1.0);
+      sum += f;
    }
    double result = step * sum;
    printf("integral is %.15g\n", result);
